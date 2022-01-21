41149519d5e6d9d243b51f3ea46667b149b8cc37
Rongrong Chai <rchai@sagebase.org>
2022-01-20 23:57:08 -0500

    add env setup

---
+++ install-pkgs.R
@@ -0,0 +1,7 @@
+if (!suppressWarnings(require("pacman", character.only = TRUE))) {
+  install.packages("pacman", repos = "https://cran.r-project.org/")
+}
+
+pkg_list <- c("reticulate", "dplyr", "glue")
+
+pacman::p_load(pkg_list, character.only = TRUE)
